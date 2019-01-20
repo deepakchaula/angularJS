01cfac102eb1df94a467ccd44bcfb8d5b1d32bc7
deepakchaula <yess_bose@yahoo.com>
2019-01-20 15:33:14 +0530

    Update firebase.js

--- scripts/utils/firebase.js
+++ scripts/utils/firebase.js
@@ -2,11 +2,11 @@
 /* jshint esversion: 6 */
 
 const config = {
-    apiKey: "AIzaSyAjQ7_WNR_lukCfDLrJ4Voet3GiNHIE55c",
-    authDomain: "onlinetestapp-b82e6.firebaseapp.com",
-    databaseURL: "https://onlinetestapp-b82e6.firebaseio.com",
-    projectId: "onlinetestapp-b82e6",
+    apiKey: "*-Uses Your-*",
+    authDomain: "*-Uses Your-*",
+    databaseURL: "*-Uses Your-*",
+    projectId: "*-Uses Your-*",
     storageBucket: "",
-    messagingSenderId: "953179765120"
+    messagingSenderId: "*-Uses Your-*"
 };
 firebase.initializeApp(config);
