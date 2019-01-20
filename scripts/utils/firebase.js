38ffbc92800fc2a6c79a0c968466222f4612c044
Deepak Chaula <deepak@mediccreations.com>
2019-01-20 15:26:34 +0530

    upload
    uploaded

---
+++ scripts/utils/firebase.js
@@ -0,0 +1,12 @@
+
+/* jshint esversion: 6 */
+
+const config = {
+    apiKey: "AIzaSyAjQ7_WNR_lukCfDLrJ4Voet3GiNHIE55c",
+    authDomain: "onlinetestapp-b82e6.firebaseapp.com",
+    databaseURL: "https://onlinetestapp-b82e6.firebaseio.com",
+    projectId: "onlinetestapp-b82e6",
+    storageBucket: "",
+    messagingSenderId: "953179765120"
+};
+firebase.initializeApp(config);
